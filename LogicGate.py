585a08471d19a17e089ca5b8783516269b99da02
chrisrivera <cr1123@gmail.com>
2022-02-17 20:33:45 -0500

    update to add switches

--- LogicGate.py
+++ LogicGate.py
@@ -23,8 +23,45 @@
 
 class LogicGate:
     def __init__(self):
-        self.led = LED(25)
-        #self.gpio25 = GPIO(25)
+        self._switch_1 = 0          #Not initialized        
+        self._switch_2 = 0          #Not initialized    
+        self._switch_3 = 0          #Not initialized    
+        GPIO.setmode(GPIO.BCM)        
+        self.led = LED(25)          #LED        
+        GPIO.setup(4,GPIO.IN)       #Switch1 UP
+        GPIO.setup(17,GPIO.IN)      #Switch1 DOWN
+        GPIO.setup(18,GPIO.IN)      #Switch2 UP
+        GPIO.setup(23,GPIO.IN)      #Switch2 DOWN        
+        GPIO.setup(16,GPIO.IN)      #Switch3 UP
+        GPIO.setup(21,GPIO.IN)      #Switch3 DOWN
+        
+    @property
+    def switch_1(self):
+        return self._switch_1
+    
+    @property
+    def switch_2(self):
+        return self._switch_2
+    
+    @property
+    def switch_3(self):
+        return self._switch_3
+                
+    def refreshState(self):        
+        if GPIO.input(4):
+            self.switch_1 = -1           #if 17 is grounded (SWITCH DOWN)
+        elif GPIO.input(17):
+            self.switch_1 = 1            #if 4 is grounded (SWITCH UP)
+        if GPIO.input(18):
+            self.switch_2 = -1           #if 23 is grounded (SWITCH DOWN)
+        elif GPIO.input(23):
+            self.switch_2 = 1            #if 17 is grounded (SWITCH UP)
+        if GPIO.input(16):
+            self.switch_3 = -1           #if 21 is grounded (SWITCH DOWN)
+        elif GPIO.input(21):
+            self.switch_3 = 1            #if 16 is grounded (SWITCH UP)
+        
+    
     
     def turnOnLED(self):
         print("turning ON LED")
@@ -47,25 +84,66 @@ class LogicGate:
             elif action == "X":
                 break
 
+       
+def switchDiagnostic(switchState):
+    if switchState[0] == 1:        
+        print("SW 1 - UP")
+    if switchState[0] == -1:        
+        print("SW 1 - DOWN")
+    if switchState[1] == 1:       
+        print("SW 2 - UP")
+    if switchState[1] == -1:        
+        print("SW 2 - DOWN")
+    if switchState[2] == 1:
+        print("SW 3 - UP")
+    if switchState[2] == -1:
+        print("SW 3 - DOWN")
+
+def getState(gate, switchState):
+    if gate.switch_1 == 1 and switchState[0] != gate.switch_1:
+        switchState[0] = gate.switch_1
+    if gate.switch_1 == -1 and switchState[0] != gate.switch_1:
+        switchState[0] = gate.switch_1        
+    if gate.switch_2 == 1 and switchState[1] != gate.switch_2:
+        switchState[1] = gate.switch_2
+    if gate.switch_2 == -1 and switchState[1] != gate.switch_2:
+        switchState[1] = gate.switch_2
+    if gate.switch_3 == 1 and switchState[2] != gate.switch_3:
+        switchState[2] = gate.switch_3
+    if gate.switch_3 == -1 and switchState[2] != gate.switch_3:
+        switchState[2] = gate.switch_3        
+            
 if __name__ == '__main__':
     import sys
+    import array
+    import RPi.GPIO as GPIO
+    
     from time import sleep
     from gpiozero import LED
     from gpiozero import Button
-        
-    button = Button(2)
+    
     gate = LogicGate()
     print("Logic Gate Started")
+    switchState = [0] * 3           #array of length 3
     
-    #gate.diagnosis()
-    
+    #gate.diagnosis()       
+    button = Button(2)              #SDA    (ACTION)
+        
     while True:
+        sleep(0.001)                #restrict clocks        
+        gate.refreshState()        
+        getState(gate, switchState)
+        
+        #switchDiagnostic(switchState)
+                
         if button.is_pressed:
-            gate.turnOnLED()
-            sleep(10)
-            gate.turnOffLED();
-        
-        
+            print('switch state:',switchState[0],switchState[1],switchState[2])            
+            if switchState[0] == 1 and switchState[1] == 1 and switchState[2] == 1:
+                print("all green, turning light on!")
+                gate.turnOnLED()
+            else:
+                gate.turnOffLED()
+            sleep(1)                #dont over press button
+
             
         
-            
